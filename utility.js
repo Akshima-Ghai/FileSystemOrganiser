e0c3d99c9fa4424e0564c40aedb369647a642fee
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2022-01-29 17:52:57 +0530

    Create utility.js

---
+++ utility.js
@@ -0,0 +1,8 @@
+let utility = {}
+utility.types = {
+    media: ["mp4", "mkv"],
+    archives: ["zip", "rar", "tar", "ar", "xz"],
+    documents: ["doc", "docx", "pdf", "xls", "xlsx", "txt"],
+    app:["exe","pkg"]
+}
+module.exports = utility;
